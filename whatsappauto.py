e00249970aa66606a8098b6bbda16140acff41c4
Yonta Josue <yontajosue@gmail.com>
2025-01-08 09:28:49 +0100

    Second commit

--- whatsappauto.py
+++ whatsappauto.py
@@ -8,20 +8,20 @@ def whatsapp_message():
         print(f"Automation started at {datetime.datetime.now()}")
         
         
-        pyautogui.press("win");time.sleep(2); pyautogui.typewrite("WhatsApp"); time.sleep(2);pyautogui.press("enter");time.sleep(2) 
+        pyautogui.press("win");time.sleep(2); pyautogui.typewrite("WhatsApp"); time.sleep(2);pyautogui.press("enter");time.sleep(100) 
 
-        pyautogui.click(x=199, y=182);time.sleep(2)
+        pyautogui.click(x=199, y=182);time.sleep(5)
 
         contact_name = "DFM"  
-        pyautogui.typewrite(contact_name);time.sleep(2);pyautogui.press("enter");time.sleep(2)
-
-        pyautogui.typewrite("Good Morning guys");pyautogui.press("enter")
+        pyautogui.typewrite(contact_name);time.sleep(2); pyautogui.click(x=217, y=279);time.sleep(2)
+        # pyautogui.press("enter");time.sleep(2)
+        pyautogui.typewrite("tsuips vas la bas c'est le automatic message qui donne, je viens d'arranger le pb ");pyautogui.press("enter")
         print(f"Message sent successfully to {contact_name}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-schedule.every().day.at("12:13").do(whatsapp_message)
+schedule.every().day.at("08:58").do(whatsapp_message)
 
 print("Scheduler is running...")
 while True:
